b81b71384c20d1010f77922619e6e23bfe8f489a
Abdul Waheed A <abdulwaheeda638@gmail.com>
2025-06-05 11:35:29 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+       System.out.println("ENTER THE AGE:");
+       Scanner ref=new Scanner(System.in);
+       int a=ref.nextInt();
+       int b=18;
+       if (a>b);
+       {
+           System.out.println("your are selected");
+       }
+       else (a<b);
+       {
+            System.out.println("your not selected");
+       }
+    }
+}
