1a9fe3c402b904bfc587649e5c9869a9a09f685a
Abdul Waheed A <abdulwaheeda638@gmail.com>
2025-06-05 12:51:56 +0530

    Create Atm.java

---
+++ Atm.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Atm {
+
+	public Atm() {
+		
+	}
+
+	public static void main(String[] args) {
+		Scanner ref=new Scanner(system.in);
+		System.out.println("enter the your pin :");
+		int pin=ref.nextInt();
+		System.out.println("enter the your pin :");
+		int amount=ref.nextInt();
+		if (amount <= 50000) {
+			
+		}
+		else {
+			//withdraw successfully
+		}
+		else {
+			//insufficent balance
+		}
+		else {
+			//invalid
+		}
+	}
+
+}
