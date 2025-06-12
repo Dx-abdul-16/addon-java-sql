3f2ac17e8999c70aa04c94886d08499bf9eb02b7
Abdul Waheed A <abdulwaheeda638@gmail.com>
2025-06-12 09:45:01 +0530

    Create Collection.java

---
+++ day5/Collection.java
@@ -0,0 +1,21 @@
+package day6;
+
+import java.util.ArrayList;
+
+public class Collection {
+
+	public static void main(String[] args) {
+		ArrayList<String> stu = new ArrayList<String>();
+		stu.add("surya");
+		stu.add(" abdul");
+		stu.add("kumar");
+		
+		stu.forEach(num->System.out.println());
+		
+		System.out.println(stu);
+		
+		
+	}
+
+}
+
