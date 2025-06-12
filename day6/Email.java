e7b2b5b7be08dcba281d634ee5f4010880263d83
Abdul Waheed A <abdulwaheeda638@gmail.com>
2025-06-12 14:06:53 +0530

    Create Email.java

---
+++ day6/Email.java
@@ -0,0 +1,15 @@
+package day6;
+
+
+import java.util.HashSet;
+ 
+public class Email {
+	public static void main(String[] args) {
+		HashSet<String> email=new HashSet<>();
+		email.add("surya23@gamil.com");
+		
+		for (String data : Email)
+		System.out.println();
+		
+	}
+}
