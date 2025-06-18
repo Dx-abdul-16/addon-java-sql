20074afcd364c39d08dc72417c24866fec8273a8
Abdul Waheed A <abdulwaheeda638@gmail.com>
2025-06-18 10:09:06 +0530

    Create q4.sql

---
+++ DAY 11/q4.sql
@@ -0,0 +1,22 @@
+mysql> select ename
+    -> from emp
+    -> where sal > (select sal from emp
+    -> where ename ='smith');
++--------+
+| ename  |
++--------+
+| ALLEN  |
+| WARD   |
+| JONES  |
+| MARTIN |
+| BLAKE  |
+| CLARK  |
+| SCOTT  |
+| KING   |
+| TURNER |
+| ADAMS  |
+| JAMES  |
+| FORD   |
+| MILLER |
++--------+
+13 rows in set (0.00 sec)
