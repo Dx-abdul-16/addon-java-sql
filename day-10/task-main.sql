b6bc5141963b3f21e9dba949f9591c0182048a74
Abdul Waheed A <abdulwaheeda638@gmail.com>
2025-06-19 14:57:00 +0530

    Create task-main.sql

---
+++ day-10/task-main.sql
@@ -0,0 +1,30 @@
+create database sales;
+
+use sales;
+
+
+CREATE TABLE Products (
+    product_id INT PRIMARY KEY AUTO_INCREMENT,
+    product_name VARCHAR(100) ,
+    price DECIMAL(10, 2)
+);
+
+CREATE TABLE Sales (
+    sale_id INT PRIMARY KEY AUTO_INCREMENT,
+    product_id INT,
+    sale_date DATE,
+    quantity INT
+  
+);
+
+INSERT INTO Products (product_name, price) VALUES
+('Laptop', 1200.00),
+('Smartphone', 800.00),
+('Tablet', 500.00);
+
+INSERT INTO Sales (product_id, sale_date, quantity) VALUES
+(1, '2025-06-01', 3),
+(2, '2025-06-01', 5),
+(1, '2025-06-02', 2),
+(3, '2025-06-02', 7);
+
