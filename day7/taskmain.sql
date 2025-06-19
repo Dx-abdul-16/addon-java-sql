e6e2621abb60a2e565c3b0fbbc762726d1aa1a0f
Abdul Waheed A <abdulwaheeda638@gmail.com>
2025-06-19 14:24:50 +0530

    Update taskmain.sql

--- day7/taskmain.sql
+++ day7/taskmain.sql
@@ -14,14 +14,6 @@ CREATE TABLE Members (
     JoinDate DATE
 );
 
-CREATE TABLE Loans (
-    LoanID INT PRIMARY KEY,
-    BookID INT,
-    MemberID INT,
-    LoanDate DATE,
-    ReturnDate DATE,
-);
-
 INSERT INTO Books (Title, Author, Genre, YearPublished) VALUES
 ('1984', 'George Orwell', 'Dystopian', 1949),
 ('To Kill a Mockingbird', 'Harper Lee', 'Classic', 1960),
@@ -47,7 +39,3 @@ INSERT INTO Members (Name, Email, JoinDate) VALUES
 
 
 
-INSERT INTO Loans (BookID, MemberID, LoanDate, ReturnDate) VALUES
-(1, 1, '2023-06-01', '2023-06-15'),
-(2, 2, '2023-06-05', NULL),
-(3, 3, '2023-06-10', '2023-06-25');
